045bc49e947113f9df3e08089fa841ba57f5cfd3
Jon Wolverton <wolverton_dot_jr_at_gmail_dot_com>
2013-08-12 11:37:36 -0700

    Remove alert message on root authentication redirect.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,9 +1,18 @@
 class RootController < ApplicationController
   respond_to :html
+  skip_before_filter :authenticate_user!
+  before_filter :soft_authenticate_user!
 
   def index
     @user = current_user
     @questions = Question.all
+    render :index
+  end
+
+  def soft_authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_url
+    end
   end
 
 end
